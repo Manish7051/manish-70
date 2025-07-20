681273ca54f95a823df91dd84d124a09407a9e63
Manish7051 <manishvarma7051@gmail.com>
2025-07-20 22:33:29 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+// ...existing code...
+const toggleBtn = document.getElementById('icon');
+const navbar = document.querySelector('.navbar');
+
+toggleBtn.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+// ...existing code...
